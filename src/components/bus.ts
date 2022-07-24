8db41dd80571ae00c611e9d17574f0da1149344c
mumu <rmli1912@foxmail.com>
2022-07-24 22:45:32 +0800

    update

--- src/components/bus.ts
+++ src/components/bus.ts
@@ -8,21 +8,24 @@ type BusClass = {
 type ParamsKey = string | number | symbol
 
 // list调度中心的类型约束
-type list = {
+type List = {
   // 这里由于key是自定义的，我们只是知道key的类型，所以需要用到ts的索引签名
-  []
+  [key: ParamsKey]: Array<Function>
 }
 
 
 class Bus implements BusClass {
+  list:List
   constructor() {
-    
+    this.list = {}
   }
 
   emit() {
 
   }
-  on() {
-
+  on(name:string, callback:Function) {
+    // 获取注册过的方法, 如果没有注册过的话，就返回一个空数组
+    let fn:Array<Function> = this.list[name] || []
+    
   }
 }
