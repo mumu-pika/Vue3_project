09ba1ef8b3cb373c05bc52cd4d17cb1cf6937446
mumu <rmli1912@foxmail.com>
2022-07-28 22:16:50 +0800

    new commit 0728

--- src/App.vue
+++ src/App.vue
@@ -1,11 +1,27 @@
 <script setup lang="ts">
-  import { ref } from 'vue'
+  import { ref, nextTick } from 'vue'
   const message = ref<string>('pika')
   const div = ref<HTMLElement>()
 
-  const change = () => {
+  const current = ref<number>(0)
+
+
+  const change = async () => {
     message.value = 'saber'
+    // 这个时候DOM还没有更新
+    // 并不会打印出更新后的数据，为此需要借助到nextTick
+    // 使得等视图更新完毕后打印出更新后的数据
+    await nextTick()
     console.log(div.value?.innerText)
+    for (let i = 0; i < 10; i++) {
+      current.value = i
+    }
+
+    for (var i = 0; i < 5; i++) {
+      setTimeout(() => {
+        console.log(i) // 输出5个5
+      }, 2000)
+    }
   }
 </script>
 
@@ -19,7 +35,7 @@
 
 <style module="xixi">
 .pikaCss {
-  color: yellowgreen;
+  color: yellowgreen; 
 }
 .pikaCss2 {
   font-size: 50px;
