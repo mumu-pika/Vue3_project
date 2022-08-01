daced87cf71d9b8fd1841216c58da3a7552dd119
mumu <rmli1912@foxmail.com>
2022-08-01 18:12:54 +0800

    update pinia

--- src/main.ts
+++ src/main.ts
@@ -1,67 +1,101 @@
+// import { createApp } from 'vue'
+// // import './style.css'
+// import App from './App.vue'
+
+// // 引入mitt库
+// import mitt from 'mitt'
+
+// // 引入全局组件
+// import Card from './components/Card/index.vue'
+
+// import './assets/css/reset.less'
+
+// // 引入animate.css
+// import 'animate.css'
+
+// // 注册插件
+// import Loading from './components/loading'
+
+
+// import ElementPlus from 'element-plus'
+// import 'element-plus/dist/index.css'
+
+
+// import {createPinia} from 'pinia'
+
+// const pinia = createPinia()
+// const Mit = mitt()
+
+// declare module 'vue' {
+//   export interface ComponentCustomProperties {
+//     $Bus: typeof Mit
+//   }
+// }
+
+// type Filter = {
+//   format: <T extends any >(str: T) => T
+// }
+
+// declare module '@vue/runtime-core' {
+//   export interface ComponentCustomProperties {
+//     $filters: Filter,
+//     $loading:{
+//       show: () => void,
+//       hide: () => void
+//     }
+//   }
+// }
+
+
+// // createApp(App).mount('#app')
+// const app = createApp(App)
+// app.config.globalProperties.$Bus = Mit
+
+// // 全局定义一个过滤器方法
+// app.config.globalProperties.$filters = {
+//   format<T>(str:T):string{
+//     return `真*${str}`
+//   }
+// }
+
+// app.use(Loading)
+// app.use(ElementPlus)
+// app.use(pinia)
+
+// app.component('Card',Card).mount('#app')
+
+
+
 import { createApp } from 'vue'
-// import './style.css'
 import App from './App.vue'
+import { createPinia, PiniaPluginContext } from 'pinia'
 
-// 引入mitt库
-import mitt from 'mitt'
-
-// 引入全局组件
-import Card from './components/Card/index.vue'
-
-import './assets/css/reset.less'
-
-// 引入animate.css
-import 'animate.css'
-
-// 注册插件
-import Loading from './components/loading'
+// 定义存储state的函数
+const setStorage = (key: string, value: any) => {
+  // 这里value存储成字符串的形式
+  localStorage.setItem(key, JSON.stringify(value))
+}
 
 
-import ElementPlus from 'element-plus'
-import 'element-plus/dist/index.css'
 
+// 在main.ts中定义插件
+const piniaPlugin = (context: PiniaPluginContext) => {
+  // console.log(context)
+  // 解构赋值出store
+  let { store } = context
 
-import {createPinia} from 'pinia'
+  // store订阅装填
+  store.$subscribe(() => {
+
+  })
+  console.log(store)
+}
 
 const pinia = createPinia()
-const Mit = mitt()
 
-declare module 'vue' {
-  export interface ComponentCustomProperties {
-    $Bus: typeof Mit
-  }
-}
+// 注册pinia的插件
+pinia.use(piniaPlugin)
 
-type Filter = {
-  format: <T extends any >(str: T) => T
-}
-
-declare module '@vue/runtime-core' {
-  export interface ComponentCustomProperties {
-    $filters: Filter,
-    $loading:{
-      show: () => void,
-      hide: () => void
-    }
-  }
-}
-
-
-// createApp(App).mount('#app')
 const app = createApp(App)
-app.config.globalProperties.$Bus = Mit
-
-// 全局定义一个过滤器方法
-app.config.globalProperties.$filters = {
-  format<T>(str:T):string{
-    return `真*${str}`
-  }
-}
-
-app.use(Loading)
-app.use(ElementPlus)
 app.use(pinia)
-
-app.component('Card',Card).mount('#app')
-
-
+app.mount('#app')
--- src/store/index.ts
+++ src/store/index.ts
@@ -1,23 +1,70 @@
 import { defineStore } from 'pinia'
-
 import { Names } from './store-name'
+type User = {
+  name: string,
+  age: number
+}
+
+// 这里设置一个change之后的user
+// 同步写法
+const Runner:User = {
+  name:'xixi',
+  age: 20
+}
+
+
+// 下面模拟实现异步修改state
+// const Login = (): Promise<User> => {
+//   return new Promise((resolve) => {
+//     setTimeout(() => {
+//       resolve({
+//         name: 'xixi',
+//         age: 20
+//       })
+//     }, 2000)
+//   })
+// }
 
 export const useTestStore = defineStore(Names.TEST, {
   state: () => ({
-    name: 'pika',
-    age: 18
+    user: <User>{
+      name: 'pika',
+      age: 18
+    },
+    name: 'Heisenberg'
   }),
 
   // computed 可以修饰一些值
   getters: {
-      
+    newName():string {
+      return `${this.name}-${this.getUserAge}`
+    },
+    getUserAge():number {
+      return this.user.age
+    }
   },
 
   // methods 可以做同步和异步的操作，提交state
   actions: {
+    //同步写法
+    setUser() {
+      this.user = JSON.parse(JSON.stringify(Runner))
+      this.setName('WW')
+    },
+
+    // 异步action
+    // async setUser() {
+    //   let result = await Login()
+    //   this.user = result
+    //   this.setName('WW')
+    // },
+
     setName(name:string) {
       this.name = name
+    },
+
+    setAge() {
+      this.user.age ++
     }
   }
-
 })
